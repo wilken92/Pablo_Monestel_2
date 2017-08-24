97459b0cc45a2f6864074ead1d53ab74dc296a0d
wilken92 <wchacong@ucenfotec.ac.cr>
2017-08-24 15:51:14 -0600

    backend

---
+++ public/route.js
@@ -0,0 +1,61 @@
+(function(){
+  'use strict';
+  angular
+  .module('testRoutes', ['ui.router', 'oc.lazyLoad', 'angularCSS','ngMessages'])
+  .config(configuration)
+
+
+  configuration.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+  function configuration($stateProvider, $urlRouterProvider){
+    $stateProvider
+
+    .state('landing',{
+      url : '/landing',
+      templateUrl: './components/landing/landing.view.html',
+      css:'css/style.css'
+    })
+
+    .state('players',{
+      url : '/players',
+      templateUrl: './components/players/players.view.html',
+      resolve: {
+        load: ['$ocLazyLoad', function($ocLazyLoad){
+          return $ocLazyLoad.load('./components/players/players.controller.js')
+        }]
+      },
+      controller: 'playersController',
+      controllerAs: 'vm',
+      css:'css/style.css'
+    })
+
+    .state('property',{
+      url : '/property',
+      templateUrl: './components/property/property.view.html',
+      resolve: {
+        load: ['$ocLazyLoad', function($ocLazyLoad){
+          return $ocLazyLoad.load('./components/property/property.controller.js')
+        }]
+      },
+      controller: 'propertyController',
+      controllerAs: 'vm',
+      css:'css/style.css'
+    })
+
+    .state('buy',{
+      url : '/buy',
+      templateUrl: './components/buy/buy.view.html',
+      resolve: {
+        load: ['$ocLazyLoad', function($ocLazyLoad){
+          return $ocLazyLoad.load('./components/buy/buy.controller.js')
+        }]
+      },
+      controller: 'buyController',
+      controllerAs: 'vm',
+      css:'css/style.css'
+    })
+
+
+    $urlRouterProvider.otherwise('/landing');
+  }//cierre de las rutas
+})();
